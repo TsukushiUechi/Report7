dea2910cfab2670b74755ff879892d263977794f
Tsukushi Uechi <e165756@ie.u-ryukyu.ac.jp>
2017-02-07 05:59:02 +0900

    tokuten

--- src/e165756/Slot.java
+++ src/e165756/Slot.java
@@ -2,25 +2,32 @@ package e165756;
 
 public class Slot {
     public static void main(String[] args) {
-        final double Num = 10.0;
-        final int count = 50;
-        int i,n1,n2,n3;
+        double Num = 10.0;
+        int count = 50;
+        int Lucky = 7;
+        int i, n1, n2, n3,p1 = 0;
 
-        for(i = 0;i < count;i++){
-            n1 = (int)(Math.random()*Num);
-            n2 = (int)(Math.random()*Num);
-            n3 = (int)(Math.random()*Num);
-            System.out.println(n1 +"" + n2 + "" + n3);
-            if(n1 == n2 && n1 == n3){
-                System.out.println("ゾロ目！！(" + (i+1) + "回目)");
-                break;
-            }else if(n1 == n2 || n1 == n3 || n2 == n3){
-                System.out.println("惜しい！！");
+            for (i = 0; i < count; i++) {
+                n1 = (int) (Math.random() * Num);
+                n2 = (int) (Math.random() * Num);
+                n3 = (int) (Math.random() * Num);
+                System.out.println(n1 + "" + n2 + "" + n3);
+                if (n1 == n2 && n1 == n3) {
+                    System.out.println("ゾロ目！！(" + (i + 1) + "回目)");
+                    p1 += 2;
+                    if(n1 == Lucky){
+                        System.out.println("ラッキー！！");
+                        p1 += 5;
+                    }
+                } else if (n1 == n2 || n1 == n3 || n2 == n3) {
+                    System.out.println("惜しい！！");
+                    p1 += 1;
+                }
             }
-            System.out.println();
-        }
-        if(i == count){
-            System.out.println("ダメでした");
-        }
+            if (i == count) {
+                System.out.println("終了！！");
+            }
+            System.out.println("得点は" + p1 + "です");
     }
+
 }

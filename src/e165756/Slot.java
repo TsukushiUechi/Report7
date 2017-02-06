e9d25bac2c522bb5a2f9923751dd8e29bbd997eb
Tsukushi Uechi <e165756@ie.u-ryukyu.ac.jp>
2017-02-06 16:12:49 +0900

    int

--- src/e165756/Slot.java
+++ src/e165756/Slot.java
@@ -3,6 +3,16 @@ package e165756;
 public class Slot {
     public static void main(String[] args) {
         final double Num = 10.0;
+        final int count = 50;
         int i,n1,n2,n3;
+
+        for(i = 0;i < count;i++){
+            n1 = (int)(Math.random()*Num);
+            n2 = (int)(Math.random()*Num);
+            n3 = (int)(Math.random()*Num);
+            if(n1 == n2 && n1 == n3){
+                System.out.println("ゾロ目！！(" + (i+1) + "回目)");
+            }
+        }
     }
 }
